2db150302244f8ee7cb7b8a966c53a65014b4d18
bhavya harshika <bhavyaharshika1999@gmail.com>
2021-02-04 13:57:23 +0530

    dfdfgh

--- hello_rajath.c
+++ hello_rajath.c
@@ -2,6 +2,7 @@
 int main()
 {
     printf("Hello,World!");
+<<<<<<< HEAD
     printf("This is bhavya");
     return 0;//testing
     //indra testing
@@ -12,4 +13,8 @@ int main()
 =======
     printf("hello phani");
 >>>>>>> 28c9e0af2471a7a00a8926f15f59441a882a9a9f
+=======
+ 
+    return 0;
+>>>>>>> 84ddd5661b2570383730a1df89488b40ef6684c5
 }
