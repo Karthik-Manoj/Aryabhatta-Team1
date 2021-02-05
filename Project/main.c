5406e4f25eb18e6d0df6d13edb94bfec6e053ba2
stepin104312 <69116163+stepin104312@users.noreply.github.com>
2021-02-05 14:30:59 +0530

    Update main.c

--- Project/main.c
+++ Project/main.c
@@ -3,11 +3,11 @@ int main(){
     printf("hello world\n");
     printf("Karthik Manoj\n");
     printf("Hello from Rajath!!\n");
-    printf("santhrupthi\n");
+    printf("This is santhrupthi\n");
     printf("Thanu\n");
     printf("Shubham S N\n");
     printf("this is bhavya\n");
-    printf("Nisha\n");
+    printf("This is Nisha\n");
     printf("This is sairaj\n");
     printf("phani");
     return 0;
